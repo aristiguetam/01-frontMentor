b23f8534580e72bcb770735562a85584ff2df007
aristiguetam <aristiguetam97@gmail.com>
2023-06-19 18:22:56 -0500

    gap

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -10,33 +10,36 @@ export const Hero = () => {
 
                 <Image src="/assets/images/image-web-3-desktop.jpg" alt="imagen Desktop" width={0} height={0} sizes="(100vw)" className="w-full object-cover object-center hidden md:block" />
 
-                <div className="flex flex-col mt-8 space-y-4 lg:flex-row lg:space-y-0 md:gap-4">
+                <div className="flex flex-col mt-8 space-y-4 lg:flex-row lg:space-y-0 md:gap-12">
                     <div>
-                        <h1 className="text-[#05011b] text-5xl font-bold">
+                        <h1 className="text-[#05011b] lg:text-7xl text-5xl font-bold">
                             The Bright Future of Web 3.0?
                         </h1>
                     </div>
 
                     <div>
-                        <p className="text-[#acabb1] text-ellipsis ">
+                        <p className="text-[#acabb1] text-ellipsis text-2xl ">
                             We dive into the next evolution of the web that
                             claims to put the power of the platforms back
                             into the hands of the people. But is it really
                             fulfilling its promise?
                         </p>
-
-                        <button className="text-white mt-6 text-center font-medium uppercase bg-[#f15d52] px-12 py-3 hover:bg-[#00001a]">
-                            Read more
-                        </button>
-
+                            <button 
+                            className="text-white mt-12 text-center font-medium text-xl uppercase bg-[#f15d52] px-12 py-3 lg:w-[40%] w-auto h-auto lg:h-[65px] hover:bg-[#00001a] transition-all">
+                                Read   more
+                            </button>
                     </div>
+
+
                 </div>
             </div>
 
             <div className="flex flex-col justify-center  bg-[#00001a] p-5 mt-12 md:mt-0 space-y-8  md:w-[30%]">
-                <h2 className="text-[#e6a450] font-bold text-4xl">
-                    New
-                </h2>
+                <div className="flex items-start">
+                    <h2 className="text-[#e6a450] font-bold text-4xl">
+                        New
+                    </h2>
+                </div>
 
                 <div className="flex flex-col ">
                     <span className="text-white font-medium text-xl mb-2">
